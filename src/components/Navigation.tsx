2def9828a390791d7eb27500c463ccd674eb66ed
Alexander <143816434+Alexanderms00@users.noreply.github.com>
2023-10-05 11:09:39 +0500

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,7 @@ const Navigation = () => {
 		<nav className='flex justify-between items-center h-[50px] px-5 shadow-md bg-blue-400'>
 			<h3 className=' bg-blue-400 font-bold'>Githum search</h3>
 			<span className=' bg-blue-400'>
-				<Link to='/' className='mr-2 bg-blue-400'>
+				<Link to='/githubapi' className='mr-2 bg-blue-400'>
 					Home{' | '}
 				</Link>
 				<Link to='/favorites' className=' bg-blue-400'>
